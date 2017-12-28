c699b3c70f251ff8100edbfc79ad63c819611271
Chris Griggs <griggsca91@gmail.com>
2017-12-28 11:49:07 -0500

    update

--- main.rs
+++ main.rs
@@ -1,4 +1,5 @@
-use std::io;
+use std::io::{self, Write};
+use std::process::Command;
 
 fn main() {
 	shell_loop();
@@ -6,25 +7,34 @@ fn main() {
 
 fn shell_loop() {
 	loop {
-
 		let line = read_line();
 		let args = split_line(&line);
 		let result = execute(&args);
-		println!("Howdy howdy: {:?}", args);
-
 	}
 }
 
 fn execute(args: &Vec<&str>) -> u8 {
 
+	if (args.len() == 0) {
+		return 1;
+	}
+
+	let output = Command::new(args[0])
+						.args(args[1..].iter())
+						.output()
+						.expect("failed to execute process");
+	print!("{}", String::from_utf8_lossy(&output.stdout));
 	return 1;
 }
 
 fn split_line(line: &String) -> Vec<&str> {
-	line.split_whitespace().collect()
+	return line.split_whitespace().collect();
 }
 
 fn read_line() ->  String {
+		print!("> ");
+		io::stdout().flush().unwrap();
+
 		let mut input = String::new();
 		
 		io::stdin().read_line(&mut input)
